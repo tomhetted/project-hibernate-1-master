2ca7586b94ff052623f9c11f255b7edb8051210a
tomhetted <zloyvolshebnik@g,ail.com>
2023-12-14 21:25:22 +0300

    some changes

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -25,7 +25,6 @@ public class PlayerRepositoryDB implements IPlayerRepository {
         properties.put(Environment.URL, "jdbc:p6spy:mysql://localhost:3306/rpg"); //properties.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
         properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQL8Dialect");
         properties.put(Environment.HBM2DDL_AUTO, "update");
-        properties.put(Environment.SHOW_SQL, "true");
         properties.put(Environment.USER, "root");
         properties.put(Environment.PASS, "root");
         sessionFactory = new Configuration()
